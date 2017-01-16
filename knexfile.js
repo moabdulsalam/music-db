5d49e925c40ae470038ebe9f57b95c2b89a3465a
moabdulsalam <m.salam2016@outlook.com>
2017-01-16 15:47:38 -0700

    :tada:

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/music'
+
+
+  },
+
+
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+
+  }
+
+};
